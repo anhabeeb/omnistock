634893a60f9a5fe788f7f1b0a4dd7612008819f4
anhabeeb <anhabeeb95@gmail.com>
2026-03-26 21:12:01 +0500

    Add files via upload

---
+++ src/types/html5-qrcode.d.ts
@@ -0,0 +1,16 @@
+declare module "html5-qrcode" {
+  export class Html5QrcodeScanner {
+    constructor(
+      elementId: string,
+      config: { fps: number; qrbox: { width: number; height: number } },
+      verbose?: boolean,
+    );
+
+    render(
+      onSuccess: (decodedText: string, decodedResult?: unknown) => void,
+      onError: (errorMessage: string, error?: unknown) => void,
+    ): void;
+
+    clear(): Promise<void>;
+  }
+}
