35a525051fbe16a2111fd3119089692b75fd87fd
anhabeeb <anhabeeb95@gmail.com>
2026-03-27 01:40:21 +0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "omnistock-shell-v2";
+const CACHE_NAME = "omnistock-shell-v3";
 const APP_SHELL = ["/", "/manifest.webmanifest"];
 const STATIC_DESTINATIONS = new Set(["script", "style", "font", "image"]);
 
